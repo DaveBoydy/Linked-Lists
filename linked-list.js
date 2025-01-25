39ebebf5130334ad99bdee727e0c3043b30e3467
David Boyd <dboyd2005@msn.com>
2025-01-25 12:57:10 +0000

    Add insertAt, removeAt and comments

--- linked-list.js
+++ linked-list.js
@@ -3,9 +3,10 @@ import Node from "./node.js";
 export default function LinkedList() {
   let headNode = null;
 
+  // adds a new node the end of the list
   const append = (value) => {
     if (head() === null) {
-      prepend(value);
+      headNode = Node(value);
     } else {
       let tmp = head();
       while (tmp.nextNode !== null) tmp = tmp.nextNode;
@@ -13,10 +14,15 @@ export default function LinkedList() {
     }
   };
 
+  // adds a new node to the start of the list
   const prepend = (value) => {
+    let tmp = null;
+    if (headNode != null) tmp = headNode;
     headNode = Node(value);
+    headNode.nextNode = tmp;
   };
 
+  // returns the total number of nodes in the list
   const size = () => {
     let tmp = headNode;
     let size = 0;
@@ -27,10 +33,12 @@ export default function LinkedList() {
     return size;
   };
 
+  // returns the first node in the list
   const head = () => {
     return headNode;
   };
 
+  // returns the last node in the list
   const tail = () => {
     let tmp = headNode;
     while (tmp.nextNode !== null) {
@@ -39,16 +47,17 @@ export default function LinkedList() {
     return tmp;
   };
 
+  // returns the node at the given index
   const at = (index) => {
-    let i = 1;
     let tmp = headNode;
-    while (i < index) {
-      tmp = tmp?.nextNode;
-      i++;
+    for (let i = 0; i < index; i++) {
+      tmp = tmp.nextNode;
+      if (tmp == null) return null;
     }
     return tmp;
   };
 
+  // removes the last element from the list
   const pop = () => {
     let current = headNode;
     let next = current.nextNode;
@@ -66,6 +75,7 @@ export default function LinkedList() {
     return next;
   };
 
+  // returns true if the value is in the list, else return false
   const contains = (value) => {
     let tmp = headNode;
     while (tmp !== null) {
@@ -75,6 +85,19 @@ export default function LinkedList() {
     return false;
   };
 
+  // returns the index of the node containing value, or null
+  const find = (value) => {
+    let tmp = headNode;
+    let index = 0;
+    while (tmp != null) {
+      if (tmp.value === value) return index;
+      tmp = tmp.nextNode;
+      index++;
+    }
+    return null;
+  };
+
+  // represents LinkedList objects as strings
   const toString = () => {
     let tmp = headNode;
     let string = "";
@@ -85,14 +108,57 @@ export default function LinkedList() {
     return `${string}null`;
   };
 
+  // inserts a new node with the provided value at the given index
+  const insertAt = (value, index) => {
+    if (index > size() - 1) {
+      append(value);
+      return;
+    }
+
+    if (index <= 0) {
+      prepend(value);
+      return;
+    }
+
+    const newNode = Node(value);
+    const formerNode = at(index - 1);
+
+    newNode.nextNode = formerNode.nextNode;
+    formerNode.nextNode = newNode;
+  };
+
+  // removes the node at the given index
+  const removeAt = (index) => {
+    if (headNode === null) return;
+
+    if (index <= 0) {
+      headNode = headNode.nextNode;
+      return;
+    }
+
+    if (index > size() - 1) {
+      pop();
+      return;
+    }
+
+    const latterNode = at(index + 1);
+    const formerNode = at(index - 1);
+
+    formerNode.nextNode = latterNode;
+  };
+
   return {
     append,
     prepend,
     size,
+    head,
     tail,
     at,
     pop,
     contains,
+    find,
     toString,
+    insertAt,
+    removeAt,
   };
 }
--- main.js
+++ main.js
@@ -10,15 +10,21 @@ list.append("snake");
 list.append("turtle");
 
 console.log("Size:", list.size());
+console.log("Head:", list.head());
 console.log("Tail:", list.tail());
-console.log("At:", list.at(5));
+console.log("At index:", list.at(0));
 console.log("Pop:", list.pop());
 console.log("Print list:", list.toString());
 console.log("Contains value:", list.contains("snake"));
-console.log("Contains value:", list.contains("turtle"));
+console.log("Contains value:", list.contains("monkey"));
+
+console.log("Find index value:", list.find("snake"));
 
 list.prepend("monkey");
 
+list.insertAt("horse", 3);
+
 console.log("Print list:", list.toString());
 console.log("Pop:", list.pop());
+list.removeAt(3);
 console.log("Print list:", list.toString());
